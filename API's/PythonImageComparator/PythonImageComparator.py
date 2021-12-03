dbdb1f36e6e8a2a035792e911baab3a5051511a3
Andraelity <Andraelity@gmail.com>
2021-12-03 13:56:30 -0500

    First commit

---
+++ API's/PythonImageComparator/PythonImageComparator.py
@@ -0,0 +1,49 @@
+#Como se pueden encaminar las cualidades de la mente con tal de producir acciones de caracter mayor, con tal de sintetizar 
+#Los aplicativos que mejoran como el componente intelectual se produce, como el comportamiento interno se conduce.
+#Como las experiencias de mi saber determinan como yo puedo entender, las realidades que yo quiero vivir.
+#Como aprendo a ser mas magico, como yo aprendo a ser mas alto, como yo puedo entender, las verdades que construyen el 
+#El proceso que se mueve en mi, el proceso que produce mente, las cualidades que mejoran como mi realidad se 
+#conduce a si misma, como el saber promueve como el comportamiento se analiza.
+
+
+
+#Como yo puedo conducir mi mente hacia el trabajo del saber.
+
+#Entendiendo como yo puedo hacer para construir el proceso que produce el codigo que yo quiero entender.
+
+
+#Installa OpenCV
+
+#Comienza una cosa y completala
+#Vamos a comenzar
+
+# pip install numpy
+# pip install opencv-python
+
+
+
+
+#Comparame dos imagenes y dime si son iguales
+#Describe el proceso de como hiciste el objetivo en un articulo.
+
+#El repositorio con el articulo y el programa, el programa corre cuando instales, y lees el articulo.
+
+
+#Puedo comparar imagenes, y decir si son iguales
+
+#Yo uso los scripts de python para mandar comandos al terminal.
+#Esos scripts ejecutan comandos de git.
+#Y esos comandos de git, me actualizan la nubecita.
+#Me dan la fuerza.
+#Fuerza a GitHub, que cualquier archivo que yo actualize en mi repositorio siempre se sube.
+#Yo puedo validar si el comando de git se ejecuto de manera correcta.
+#Lo siguiente seria que.
+
+
+import numpy as np
+import cv2
+
+img = cv2.imread('6.jpg', 1)
+
+print(img)
+
